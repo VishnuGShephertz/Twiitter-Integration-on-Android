fb281ace4c4aeb47b0d027be9da091e76bc6571d
Vishnu <vishnu.garg@shephertz.co.in>
2013-08-29 13:23:13 +0530

    constants file

---
+++ src/com/shephertz/android/twitter/sample/Constants.java
@@ -0,0 +1,22 @@
+package com.shephertz.android.twitter.sample;
+/*
+ * @author Vishnu Garg
+ */
+public class Constants {
+	//Your Twitter application Consumer Key and Consumer Secret 
+	static String CONSUMER_KEY = "<Your Consumer Key>";
+	static String CONSUMER_SECRET = "Your Consumer Secret key";
+
+	static String PREFERENCE_NAME = "twitter_oauth";
+	static final String PREF_KEY_SECRET = "oauth_token_secret";
+	static final String PREF_KEY_TOKEN = "oauth_token";
+	static final String PREF_KEY_USER = "twitter_name";
+	
+	static final String CALLBACK_URL = "oauth://t4jsample";
+	
+	static final String IEXTRA_AUTH_URL = "auth_url";
+	static final String IEXTRA_OAUTH_VERIFIER = "oauth_verifier";
+	static final String IEXTRA_OAUTH_TOKEN = "oauth_token";
+	
+	static final String APP_NAME="TwitterSample";
+}
