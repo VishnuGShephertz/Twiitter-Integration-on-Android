88894a4730b0a4aacf5cebef41ebeecc9c466d91
Vishnu <vishnu.garg@shephertz.co.in>
2013-09-10 14:25:27 +0530

    Update Status is added in Tweet List

--- src/com/shephertz/android/twitter/sample/Constants.java
+++ src/com/shephertz/android/twitter/sample/Constants.java
@@ -4,11 +4,11 @@ package com.shephertz.android.twitter.sample;
  */
 public class Constants {
 	//Your Twitter application Consumer Key and Consumer Secret 
-	static String CONSUMER_KEY = "<Your Consumer Key>";
-	static String CONSUMER_SECRET = "<Your Consumer Secret Key>";
+	static String CONSUMER_KEY = "zdTFTpIjc0nq5rR9VtTZMw";
+	static String CONSUMER_SECRET = "8mqHAJJL9PnSvjGfntmdJ0uWFIEILRIuRQ5jJZ34";
 
-	 static String API_KEY ="<Your APP42API Key>";
-	 static String SECRET_KEY="<Your APP42SECRET Key>";
+	 static String API_KEY ="462302e01fed25065d6cb856ddfad422a871c05f39b4193e68a78da66eaa42b1";
+	 static String SECRET_KEY="60735a72502ebff2d6b4318fed646049720063e3cdbda5d647ed27d97567e179";
 	static String PREFERENCE_NAME = "twitter_oauth";
 	static final String PREF_KEY_SECRET = "oauth_token_secret";
 	static final String PREF_KEY_TOKEN = "oauth_token";
